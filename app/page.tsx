5942da40e2a5718c7cf137d6525e127d6cf7ac38
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-05-31 01:04:54 -0300

    add Projects and Skills components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import About from "@/components/About";
 import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -8,9 +10,9 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
-      {/* <Projects />
+      <Projects />
       <Skills />
-      <Experience />
+      {/* <Experience />
       <Contact /> */}
     </main>
   );
