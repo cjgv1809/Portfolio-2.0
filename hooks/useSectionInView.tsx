626641bf0942d53a167fefcf8b2c5e98f40fbb94
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-05-31 00:43:26 -0300

    add customs hooks and context

---
+++ hooks/useSectionInView.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useActiveSection } from "./useActiveSection";
+import type { SectionName } from "@/lib/types";
+
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+  const { ref, inView } = useInView({
+    threshold,
+  });
+  const { setActiveSection, timeOfLastClick } = useActiveSection();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection(sectionName);
+    }
+  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
+
+  return {
+    ref,
+  };
+}
