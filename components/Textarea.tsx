5e949ff7c745404123d83fe718e49fa41c5bb41e
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-06-06 13:46:35 -0300

    fix types for handleMouseMove function

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -16,7 +16,11 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     const mouseX = useMotionValue(0);
     const mouseY = useMotionValue(0);
 
-    function handleMouseMove({ currentTarget, clientX, clientY }: any) {
+    function handleMouseMove({
+      currentTarget,
+      clientX,
+      clientY,
+    }: React.MouseEvent<HTMLDivElement>) {
       const { left, top } = currentTarget.getBoundingClientRect();
       mouseX.set(clientX - left);
       mouseY.set(clientY - top);
